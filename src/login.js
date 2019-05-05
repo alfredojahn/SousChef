2ffd3ba3935edf0c67549b8c140d07675fe5301e
paycheck97 <43653779+paycheck97@users.noreply.github.com>
2019-05-05 10:16:02 -0400

    kk

---
+++ src/login.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './login.css';
+
+function Login() {
+    return (
+        <>
+        <div id="heading">
+            <h1 id="loginBox">Sous-Chef</h1>
+        </div>
+        <div class="pure-g" id="loginBox">
+            <div class="pure-u-1-2"><p>Thirds</p></div>
+            <div class="pure-u-1-3"><p>Thirds</p></div>
+        </div>
+
+        </>
+    );
+  }
+  
+  export default Login;
