102adbc9c18f3d8869a7e1ce5e0001a46c185839
paycheck97 <43653779+paycheck97@users.noreply.github.com>
2019-05-14 16:09:05 -0400

    Update
    User view en proceso + cambios en el login

--- src/components/user.js
+++ src/components/user.js
@@ -1,11 +1,31 @@
 import React from 'react'
+import './user.css';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
 
 
 const User = () =>{
+    const bull = <span >•</span>;
     return(
-        <div>
-            <h2>User Page</h2>
-        </div>
+        <Card id="carta">
+            <CardContent>
+                <Typography  color="textSecondary" gutterBottom>
+                    Pagina de Usuario
+                </Typography>
+                <Typography variant="h5" component="h2">
+                User{bull}Name </Typography>
+                <Typography component="p">
+                "No hay amor mas sincero <br/>que el amor a la comida"
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">Edit Username</Button>
+            </CardActions>
+        </Card>
     )
 }
 
